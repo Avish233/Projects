021e5114d222afdc582fb88f30a4c8d399f796ff
Avish233 <avishpamah09@hotmail.com>
2025-11-26 22:56:18 +0400

    Remove API key from .env file and update comments in weather.py for clarity

--- weather_project/weather_cli/weather.py
+++ weather_project/weather_cli/weather.py
@@ -1,11 +1,16 @@
 import requests
 import os
 from dotenv import load_dotenv
-
+#--------------------------------
+# Load .env file
+#--------------------------------
 load_dotenv()
 
+
+# Make a GET request to the OpenWeatherMap API
 weather = requests.get(
     "http://api.openweathermap.org/data/2.5/weather?q=Mauritius,mu&appid=" + os.getenv("api_key")
 )
 
+# Print the JSON response
 print(weather.json())
